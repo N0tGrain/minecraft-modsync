78033d2f2873c569510100ea5b59241fba8233eb
Kaylee Hoogwerf <s1152274@student.hsleiden.nl>
2026-04-18 15:28:26 +0200

    Initialize backend and frontend structure

---
+++ modsync-backend/src/main/java/com/n0tgrain/modsyncbackend/ModsyncBackendApplication.java
@@ -0,0 +1,13 @@
+package com.n0tgrain.modsyncbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModsyncBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModsyncBackendApplication.class, args);
+	}
+
+}
---
+++ modsync-backend/src/test/java/com/n0tgrain/modsyncbackend/ModsyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n0tgrain.modsyncbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModsyncBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
