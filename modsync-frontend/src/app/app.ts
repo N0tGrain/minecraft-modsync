26391e9e8c3f4fedf59365aaf4c7c302d8932995
Kaylee Hoogwerf <s1152274@student.hsleiden.nl>
2026-06-09 20:07:10 +0200

    Feat: added responsive navbar

--- modsync-frontend/src/app/app.ts
+++ modsync-frontend/src/app/app.ts
@@ -1,15 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
 
 /**
  * Root component of the application.
  */
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal<string>('modsync-frontend');
+  protected isLoggedIn: boolean = false; //TODO: MOVE THIS TO AUTHENTICATION SERVICE
 }
---
+++ modsync-frontend/src/app/components/navbar/navbar.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
+})
+export class Navbar {
+
+  protected isMenuOpen: boolean = false;
+
+  protected toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  protected closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+
+}
